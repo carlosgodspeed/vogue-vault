a6739c005a2bb713c572b9738f54eb98ccbaa17c
carlosgodspeed <carlosminibics@gmail.com>
2023-10-18 14:14:25 -0300

    desenvolvimento da loja vogue vault

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link re="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div class="background">
+        <header>
+            <div class="logo">
+                <h1>Vogue Vault</h1>
+            </div><!--logo-->
+
+            <div class="cabeçalho-link">
+                <li>
+                    <a href="index.php">Inicio</a>
+                </li>
+                <li>
+                    <a href="Routes/Novidades/Novidade.php">Novidades</a>
+                </li>
+                <li>
+                    <a href="Routes/Closet/Closet.php">Closet</a>
+                </li>
+                <li>
+                    <a href="Routes/Novidades/Novidade.php">Contato</a>
+                </li>
+            </div><!--cabeçalho-link-->
+            <div class="icon"><span><ion-icon name="bag-handle-outline"></ion-icon></span></div><!--icon-->
+        </header>
+    </div><!--background-->
+
+    <section>
+        <h1>NOVAS CATEGORIAS</h1>
+        <div class="Container-card-1">
+            <div class="cards">
+                <a href="URL_DO_DESTINO">
+                    <img src="img/casaco-marron.png">
+                </a>
+            </div><!--cards-->
+
+            <div class="cards">
+                <a href="">
+                    <img src="img/blusa-branca.png">
+                </a>
+            </div><!--cards-->
+
+            <div class="cards">
+                <a href="URL_DO_DESTINO">
+                    <img src="img/blusa-canva.png">
+                </a>
+            </div><!--cards-->
+        </div><!--Container-card-1-->
+
+        <div class="Container-card-1">
+            <div class="cards">
+                <a href="URL_DO_DESTINO">
+                    <img src="img/casaco-marron.png">
+                </a>
+            </div><!--cards-->
+
+            <div class="cards">
+                <a href="URL_DO_DESTINO">
+                    <img src="img/casaco-marron.png">
+                </a>
+            </div><!--cards-->
+
+            <div class="cards">
+                <a href="URL_DO_DESTINO">
+                    <img src="img/casaco-marron.png">
+                </a>
+            </div><!--cards-->
+        </div><!--Container-card-1-->
+    </section>
+
+    <section class="cta">
+    </section><!--cta-->
+
+    <section>
+        <h1>ESTOQUE NOVOS</h1>
+        <div class="Container-roupas">
+            <div class="roupa">
+                <img src="img/casaco-marron.png" alt="">
+                <button style="font-size: 20px;">Comprar</button>
+            </div><!--roupa-->
+
+            <div class="roupa">
+                <img src="img/casaco-marron.png" alt="">
+                <button style="font-size: 20px;">Comprar</button>
+            </div><!--roupa-->
+
+            <div class="roupa">
+                <img src="img/casaco-marron.png" alt="">
+                <button style="font-size: 20px;">Comprar</button>
+            </div><!--roupa-->
+
+            <div class="roupa">
+                <img src="img/casaco-marron.png" alt="">
+                <button style="font-size: 20px;">Comprar</button>
+            </div><!--roupa-->
+        </div><!--container-roupas-->
+    </section>
+
+
+    <div class="Marcas">
+        <img src="img/brand1.png" alt="Marca">
+        <img src="img/brand2.png" alt="Marca">
+        <img src="img/brand3.png" alt="Marca">
+        <img src="img/brand4.png" alt="Marca">
+        <img src="img/brand5.png" alt="Marca">
+        <img src="img/brand6.png" alt="Marca">
+    </div><!--Marcas-->
+    <br>
+    <br>
+
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
